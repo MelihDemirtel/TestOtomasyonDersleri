92d2b8e902fd5fa87a34582e8987e1566832f292
MelihDemirtel <melihcandemirtel@gmail.com>
2024-03-13 22:24:16 +0300

    created description of codes

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -11,17 +11,22 @@ import java.time.Duration;
 public class Test {
     public static void main(String[] args){
 
+        //Chrome browserın driverını kurar
         WebDriver driver = new ChromeDriver();
 
+        //Bekleme işlemleri için wait nesnesi oluşturduk
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
 
-
+        //Yazılan URL e gider
         driver.get("https://demoqa.com/");
 
+        //Ekranı tam ekran yapar
         driver.manage().window().maximize();
 
+        //driver.findelement ile elementi buluyoruz ve elementButton adında bir WebElemente tanımladık.
         WebElement elementButton = driver.findElement(By.xpath("//h5[text()='Elements']"));
 
+        //Elemente tıklar
         elementButton.click();
     }
 }
